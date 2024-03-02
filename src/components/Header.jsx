aac5376424648d8d6b721ae9917df61ce124b4fc
hsh <hsh200315@naver.com>
2024-03-02 18:02:40 +0900

    feat: 헤더 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
 import { BsChevronLeft } from "react-icons/bs";
 import { CiSquarePlus } from "react-icons/ci";
 
 import './Header.css'
 
 export default function CommonHeader(props){
-	const onClickButton = () => {
-		// navigate('/setting/character', {state: {userId: userId}});
+	const navigate = useNavigate();
+
+	const onClickBack = () => {
+		navigate(-1);
+	}
+
+	const onClickCreate = (userId) => {
+		navigate('/character/setting', {state:{userId:userId}})
 	}
 
 	return(
 		<div className="HeaderContainer">
-			<BsChevronLeft size={28} onClick={onClickButton}/>
+			{props.isCharacter? <div/> : <BsChevronLeft size={28} onClick={onClickBack}/>}
 			<h2 className="text">{props.content}</h2>
-			{props.isCharacter ? <CiSquarePlus size={36}/> : <div/>}
+			{props.isCharacter ? <CiSquarePlus size={36} onClick={() => onClickCreate(props.userId)}/> : <div/>}
 		</div>
 	)
 	
